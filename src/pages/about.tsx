d4e5db0a11d14ba0cde4474924f6701c6063521d
bsitruk <bsitruk@bigid.com>
2023-08-30 11:49:30 +0300

    cleanup blitz page

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import { BlitzPage } from "@blitzjs/next";
+
+type AboutProps = {};
+
+export const AboutPage: BlitzPage = ({}: AboutProps) => {
+  return <div>This is the about page</div>;
+};
+
+export default AboutPage;
