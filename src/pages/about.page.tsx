b618c79a8af950cf95b1ea744257bbe2adc4dd93
bsitruk <bsitruk@bigid.com>
2023-09-10 15:54:49 +0300

    Simplify layout

--- src/pages/about.page.tsx
+++ src/pages/about.page.tsx
@@ -1,9 +1,14 @@
+import Layout from "@/core/layouts/Layout";
 import { BlitzPage } from "@blitzjs/next";
 
 type AboutProps = {};
 
 export const AboutPage: BlitzPage = ({}: AboutProps) => {
-  return <div>This is the about page</div>;
+  return (
+    <Layout title="About">
+      <div>This is the about page</div>
+    </Layout>
+  );
 };
 
 export default AboutPage;
